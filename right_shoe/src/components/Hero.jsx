dee5df835eb3323f7a09d5a5135651ca085aec69
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-02-13 02:03:57 +0530

    Hero Section Updated

--- right_shoe/src/components/Hero.jsx
+++ right_shoe/src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import {useState, useEffect} from 'react'
+import { FaArrowCircleRight } from "react-icons/fa";
+
 
 const Hero = () => {
     const [imageUrl, setImageUrl] = useState('./shoe1.png');
@@ -10,17 +12,38 @@ const Hero = () => {
 
   return (
     <div>
-    <div id='home'>
-        <section>
-            <p>Our Summer Collections</p>
-            <h1>
-                <span>The New Arival</span>
-                <span>Right Shoes</span>
+    <div id='home' className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-y-10 container mx-auto">
+        <section className="relative z-10 xl:w-2/5 flex flex-col justify-center items-start px-8 pt-28">
+            <p className="text-xl text-[#ff6452]">Our Summer Collections</p>
+            <h1 className="mt-10 font-palanquin text-black text-8xl max-sm:text-[72px] leading-[106px] max-sm:leading-[82px] font-bold">
+                <span className="bg-white relative z-10 pr-10 lg:text-8xl font-palanquin xl:whitespace-nowrap">The New Arival</span>{" "}
+                <br />
+                <span className="text-[#ff6452] inline-block mt-3">Right Shoes</span>
             </h1>
 
-            <p>
+            <p className="text-[#6D6D6D] text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
                 Discover stylish Right shoe arrivals, quality comfort and innovation for your active life...
             </p>
+
+            <button className=" flex justify-center items-center gap-2 px-7 py-4 border-none text-lg leading-none rounded-full bg-[#FF6452] text-white hoverBtn">
+                Shop Now
+                <FaArrowCircleRight className="ml-2 rounded-full text-2xl" />
+            </button>
+
+            <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+                <div>
+                    <p className="text-5xl font-mono font-bold text-black">1k+</p>
+                    <p className="leading-7 font-mono text-[#6D6D6D] font-bold">Brands</p>
+                </div>
+                <div>
+                    <p className="text-5xl font-mono font-bold text-black">200+</p>
+                    <p className="leading-7font-mono text-[#6D6D6D] font-bold">Shops</p>
+                </div>
+                <div>
+                    <p className="text-5xl font-mono font-bold text-black">450k+</p>
+                    <p className="leading-7 font-mono text-[#6D6D6D] font-bold">Customers</p>
+                </div>
+            </div>
         </section>
     </div>
       
